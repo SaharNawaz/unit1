215d96142778a36992296f804797811eae1851ea
SaharNawaz <102963858+SaharNawaz@users.noreply.github.com>
2022-04-18 10:59:11 +0500

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,43 @@
+#ex1
+msg_sahar='hello'
+print(msg_sahar)
+#ex2
+msg_sahar='i love Pakistan'
+print(msg_sahar)
+msg_sahar='i like python'
+print(msg_sahar)
+#ex3
+name_sahar='Sahar'
+print('Hy'+name_sahar+','+'Have you completed your homework?')
+#ex4
+name_sahar='sahar nawaz'
+print(name_sahar.lower())
+print(name_sahar.upper())
+print(name_sahar.title())
+#ex5
+msg_sahar='Life is like riding a bicycle.To keep your balance you must keep moving'
+name_sahar='Albert Einstein'
+print(name_sahar+',says!'+msg_sahar)
+#ex6
+message_sahar='life is like riding a bicycle.To keep your balance you must keep moving'
+famous_sahar='Albert Einstein'
+print(famous_sahar+',says!'+message_sahar)
+#ex7
+nm_sahar=' sahar '
+print(nm_sahar.lstrip())
+print(nm_sahar.rstrip())
+print(nm_sahar.strip())
+#ex8
+print(4+4)
+print(9-1)
+print(2*4)
+print(8/1)
+#ex9
+number_sahar='4'
+message='My favorite number is'
+print(message+number_sahar+'.')
+#ex10
+#sahar print(4+4)
+print(9-1)
+print(2*4)
+print(8/1)
